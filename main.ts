73bdce399da7183bea2a79b77f4c2fad8ee6c895
FernandoBalderas <fernandoed.bm@gmail.com>
2024-12-22 22:43:18 -0600

    feat: add quote generation

--- main.ts
+++ main.ts
@@ -1,12 +1,44 @@
 import { route, type Route } from "@std/http";
 import nacl from "https://esm.sh/tweetnacl@v1.0.3?dts";
 
-const PUBLIC_KEY = "f56b84d8db9a8b4e06f727e5243515262dac94a0b666e6e23dcf469f2ed269b0";
+const PUBLIC_KEY =
+  "f56b84d8db9a8b4e06f727e5243515262dac94a0b666e6e23dcf469f2ed269b0";
+const INTERACTION_TYPE = {
+  PING: 1,
+  APPLICATION_COMMAND: 2,
+};
 
 function hexToUint8Array(hex: string) {
-  return new Uint8Array(
-    hex.match(/.{1,2}/g)!.map((val) => parseInt(val, 16)),
+  return new Uint8Array(hex.match(/.{1,2}/g)!.map((val) => parseInt(val, 16)));
+}
+
+function buildCallbackQuery(
+  interactionID: string,
+  interactionToken: string,
+  body: any
+) {
+  return fetch(
+    `https://discord.com/api/v10/interactions/${interactionID}/${interactionToken}/callback`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    }
   );
+}
+
+async function getQuote() {
+  const res = await fetch(
+    "http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en"
+  );
+  const body = await res.json();
+
+  return {
+    text: body.quoteText,
+    author: body.quoteAuthor,
+  };
 }
 
 const routes: Route[] = [
@@ -22,7 +54,7 @@ const routes: Route[] = [
     method: "POST",
     pattern: new URLPattern({ pathname: "/api" }),
     handler: async (req) => {
-      console.log(req)
+      // console.log(req);
 
       const signature = req.headers.get("X-Signature-Ed25519")!;
       const timestamp = req.headers.get("X-Signature-Timestamp")!;
@@ -31,10 +63,10 @@ const routes: Route[] = [
       const isVerified = nacl.sign.detached.verify(
         new TextEncoder().encode(timestamp + body),
         hexToUint8Array(signature),
-        hexToUint8Array(PUBLIC_KEY),
+        hexToUint8Array(PUBLIC_KEY)
       );
 
-      if(!isVerified) {
+      if (!isVerified) {
         return new Response(
           JSON.stringify({
             type: 4,
@@ -46,28 +78,55 @@ const routes: Route[] = [
             status: 401,
             headers: new Headers({ "Content-Type": "application/json" }),
           }
-        )
+        );
       }
-      
-      return new Response(
-        JSON.stringify({
-          type: 1,
-        }),
-        {
-          status: 200,
-          headers: new Headers({ "Content-Type": "application/json" }),
+
+      const parsedBody = JSON.parse(body);
+      // console.log(parsedBody);
+
+      switch (parsedBody.type) {
+        case INTERACTION_TYPE.PING:
+          return new Response(
+            JSON.stringify({
+              type: 1,
+            }),
+            {
+              status: 200,
+              headers: new Headers({ "Content-Type": "application/json" }),
+            }
+          );
+        case INTERACTION_TYPE.APPLICATION_COMMAND: {
+          setTimeout(async () => {
+            const quote = await getQuote();
+
+            await buildCallbackQuery(
+              parsedBody.id,
+              parsedBody.token,
+              {
+                type: 4,
+                data: {
+                  content: "",
+                  embeds: [
+                    {
+                      title: `" ${quote.text} "`,
+                      description: `-${quote.author}`,
+                    },
+                  ],
+                },
+              }
+            );
+          }, 0);
+
+          return new Response(JSON.stringify(undefined), {
+            status: 202,
+            headers: new Headers({ "Content-Type": "application/json" }),
+          });
         }
-      )
-    }
+        default:
+          return new Response(undefined, { status: 404 });
+      }
+    },
   },
-  // {
-  //   pattern: new URLPattern({ pathname: "/" }),
-  //   handler: (req) =>
-  //     new Response("Hello World", {
-  //       status: 200,
-  //       headers: new Headers({ "Content-Type": "text/plain" }),
-  //     }),
-  // },
 ];
 
 Deno.serve(route(routes, () => new Response("Not Found", { status: 404 })));
